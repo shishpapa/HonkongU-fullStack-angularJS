6e3fed33aeae47931c9236ba18ceab88312f3841
Abhay Sathe <abhay.sathe@ca.com>
2016-10-06 21:20:05 -0600

    Use UI Router to get templates. Setup backed JSON DB. Retrieve data using RESTful services with ngResource

--- conFusion/web/app/scripts/controllers.js
+++ conFusion/web/app/scripts/controllers.js
@@ -7,9 +7,18 @@ angular.module('confusionApp')
         $scope.tab = 1;
         $scope.filtText = '';
         $scope.showDetails = false;
+        $scope.showMenu = false;
+        $scope.message = "Loading ...";
 
-        $scope.dishes = menuFactory.getDishes();
-
+        $scope.dishes = menuFactory.getDishes().query(
+            function (response) {
+                $scope.dishes = response;
+                $scope.showMenu = true;
+            },
+            function (response) {
+                $scope.message = "Error: " + response.status + " " + response.statusText;
+            }
+        );
 
         $scope.select = function (setTab) {
             $scope.tab = setTab;
@@ -69,38 +78,55 @@ angular.module('confusionApp')
     }])
 
     .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function ($scope, $stateParams, menuFactory) {
-
-        var dish = menuFactory.getDish(parseInt($stateParams.id, 10));
-
-        $scope.dish = dish;
-
+        $scope.dish = {};
+        $scope.showDish = false;
+        $scope.message="Loading ...";
+        $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)})
+            .$promise.then(
+                function(response){
+                    $scope.dish = response;
+                    $scope.showDish = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                }
+            );
     }])
 
-    .controller('DishCommentController', ['$scope', function ($scope) {
+    .controller('DishCommentController', ['$scope', 'menuFactory', function ($scope, menuFactory) {
 
-        $scope.mycomment = {rating: 5, comment: "", author: "", date: ""};
+        $scope.comment = {rating: 5, comment: "", author: "", date: ""};
 
         $scope.submitComment = function () {
+            $scope.comment.date = new Date().toISOString();
+            console.log($scope.comment);
+            $scope.dish.comments.push($scope.comment);
 
-            $scope.mycomment.date = new Date().toISOString();
-            console.log($scope.mycomment);
-
-            $scope.dish.comments.push($scope.mycomment);
-
+            menuFactory.getDishes().update({id:$scope.dish.id},$scope.dish);
             $scope.commentForm.$setPristine();
-
-            $scope.mycomment = {rating: 5, comment: "", author: "", date: ""};
+            $scope.comment = {rating:5, comment:"", author:"", date:""};
         };
     }])
 
     // implement the IndexController and About Controller here
-    .controller('AboutController', ['$scope', 'corporateFactory', function($scope, corporateFactory){
+    .controller('AboutController', ['$scope', 'corporateFactory', function ($scope, corporateFactory) {
         $scope.leaders = corporateFactory.getLeaders();
     }])
 
-    .controller('IndexController', ['$scope','corporateFactory', 'menuFactory' , function($scope, corporateFactory, menuFactory){
+    .controller('IndexController', ['$scope', 'corporateFactory', 'menuFactory', function ($scope, corporateFactory, menuFactory) {
         $scope.leader = corporateFactory.getLeader(3);
-        $scope.dish = menuFactory.getDish(0);
+        $scope.showDish = false;
+        $scope.message="Loading ...";
+        $scope.dish = menuFactory.getDishes().get({id:0})
+            .$promise.then(
+                function(response){
+                    $scope.dish = response;
+                    $scope.showDish = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                }
+            );
         $scope.promotion = menuFactory.getPromotion(0);
     }])
 
